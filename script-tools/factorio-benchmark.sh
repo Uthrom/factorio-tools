d9962de0acdeef9c17d8d2e4b1d7ab14af158a59
Uthrom <>
2019-09-02 13:35:49 -0500

    Added shell scripts

---
+++ script-tools/factorio-benchmark.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+FACTORIORC="$HOME/.factoriorc"			# Local settings here
+FACTORIO=""
+
+if [ -f $FACTORIORC ]; then			# Find user .rcfile and load
+  source $HOME/.factoriorc			# settings from there
+fi
+
+function usage {
+  cat << __EOF
+USAGE: `basename $0` [ -h | -m <NUM> | -t <NUM> | -r <NUM> | ] -o <OUTFILE> save1 save2 .. saveN
+
+ -h         	Displays this help
+ -m <NUM>		Number of minutes of game time to run each benchmark
+ -t <NUM>		Number of ticks to run each benchmark
+ -r <NUM>		Number of times to run benchmark
+ -o <OUTFILE>	Output to <OUTFILE> instead of stdout
+
+This script used to simplify running factorio(1) benchmarks, it lets you specify the list of save files to run, how many minutes (or ticks) to run each file, and how often to re-run the benchmark, if anything.
+
+The output is saved to the output file specified.
+__EOF
+  exit 1
+}
+
+B_MINS=0
+B_TICKS=`echo "$B_MINS * 3600" | bc`
+B_RUNS=0
+OUTPUT="benchmark-out"
+
+while getopts "hm:t:r:o:" OPTION; do
+  case $OPTION in
+    h)
+      usage
+      ;;
+    m)
+      B_MINS=$OPTARG
+      ;;
+    t)
+      B_TICKS=$OPTARG
+      ;;
+    r)
+      B_RUNS=$OPTARG
+      ;;
+    o)
+      OUTPUT=$OPTARG
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+if [ $# -lt 1 ]; then
+  echo "Not enough arguments"
+  usage
+fi
+
+if [ "x" == "x$OUTPUT" ]; then
+  OUTPUT="`echo $1 | sed -e 's/\.zip//'`"
+fi
+
+if [ $B_TICKS -lt 1 ] && [ $B_MINS -gt 0 ]; then
+  B_TICKS=`echo "$B_MINS * 3600" | bc`
+fi
+
+for FILE in $*; do
+  if [ -f $FILE ]; then
+    (
+      /bin/echo -n "$FILE,"
+      $FACTORIO --benchmark $FILE --benchmark-ticks $B_TICKS --benchmark-runs $B_RUNS 2>&1 | awk "/Performed.*updates/ {print \$5}" | xargs echo  | sed -e 's/ /,/g'
+    ) | sed -e 's/\n//g' >> $OUTPUT.csv
+  else
+    echo "File '$FILE' not found!"
+  fi
+done
---
+++ script-tools/factorio-mod-switch.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+FACTORIO_RUN=0
+FACTORIORC="$HOME/.factoriorc"                  # Local settings here
+FACTORIO=""
+STOCK_MODS=""
+FACTORIO_DATA="$HOME/Library/Application Support/factorio"
+FACTORIO_MODS="$FACTORIO_DATA/mods"
+
+if [ -f $FACTORIORC ]; then			# Find user .rcfile and load 
+  source $HOME/.factoriorc			# settings from there
+fi
+
+function usage {
+  cat << __EOF
+USAGE: `basename $0` [ -r | -h ] moddir_suffix
+
+ -r         Launches factorio (if found)
+ -h         Displays this help
+
+This script is used to re-symlink factorio mod folders on Mac OS X,
+since the Mac version of factorio(1) doesn't have a good way to run
+multiple installs.
+__EOF
+  exit 1
+}
+
+while getopts "hr" OPTION; do
+  case $OPTION in
+    h)
+      usage
+      ;;
+    r)
+      FACTORIO_RUN=1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+if [ "x" != "x$1" ]; then
+  NEW_MODS="$1"
+else
+  echo "No mod directory passed, using '$STOCK_MODS'"
+  NEW_MODS="$STOCK_MODS" 
+fi
+
+if [ "x" == "x$NEW_MODS" ]; then
+  usage
+fi 
+
+if [ "$FACTORIO_MODS-$NEW_MODS" -ef "$FACTORIO_MODS" ] ; then
+  echo "Mod directory '$NEW_MODS' already linked."
+else
+  if [ -d "$FACTORIO_MODS-$NEW_MODS" ]; then
+    rm -f "$FACTORIO_MODS"
+
+    ln -s "$FACTORIO_MODS-$NEW_MODS" "$FACTORIO_MODS"
+
+    ls -Fl "$FACTORIO_DATA" | grep 'mods'
+
+    echo "Mod directory '$NEW_MODS' linked."
+  else
+    echo "No mod directory '$NEW_MODS' found."
+    echo "Terminating"
+    exit 1
+  fi
+fi
+
+if [ $FACTORIO_RUN -gt 0 ]; then
+  echo "Launching $FACTORIO"
+  $FACTORIO
+fi
