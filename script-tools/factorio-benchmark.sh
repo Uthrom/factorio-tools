4b55cf4c013ca4a5c24e482d67840ab1512dbde0
Uthrom <>
2019-09-03 13:28:18 -0500

    Refactor display/copy code, set up cleanup traps

--- script-tools/factorio-benchmark.sh
+++ script-tools/factorio-benchmark.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+TMP="/tmp/`basename $0`.$$"			# Temp file
+trap 'rm -f $TMP' 0 1 9 11 15			# Clean up on exit
+
+rm -f $TMP					# Did somebody else leave this?
+touch $TMP					# Now it's ours
+
 FACTORIORC="$HOME/.factoriorc"			# Local settings here
-FACTORIO=""
+FACTORIO=""					# Default executable
 
 if [ -f $FACTORIORC ]; then			# Find user .rcfile and load
   source $HOME/.factoriorc			# settings from there
@@ -28,7 +34,7 @@ __EOF
 
 B_MINS=0
 B_TICKS=`echo "$B_MINS * 3600" | bc`
-B_RUNS=0
+B_RUNS=1
 OUTPUT=""
 
 while getopts "hm:t:r:o:" OPTION; do
@@ -63,18 +69,18 @@ fi
 
 for FILE in $*; do
   if [ -f $FILE ]; then
-    if [ "x" == "x$OUTPUT" ]; then
-      (
-        /bin/echo -n "$FILE,"
-        $FACTORIO --benchmark $FILE --benchmark-ticks $B_TICKS --benchmark-runs $B_RUNS 2>&1 | awk "/Performed.*updates/ {print \$5}" | xargs echo  | sed -e 's/ /,/g'
-      ) | sed -e 's/\n//g'
-    else
       ( 
         /bin/echo -n "$FILE," 
         $FACTORIO --benchmark $FILE --benchmark-ticks $B_TICKS --benchmark-runs $B_RUNS 2>&1 | awk "/Performed.*updates/ {print \$5}" | xargs echo  | sed -e 's/ /,/g'
-      ) | sed -e 's/\n//g' >> $OUTPUT.csv
-    fi
+      ) | sed -e 's/\n//g' >> $TMP
   else
     echo "File '$FILE' not found!"
   fi
 done
+
+# Display results from above
+if [ "x" == "x$OUTPUT" ]; then
+  cat $TMP
+else
+  cp $TMP $OUTPUT.csv
+fi
